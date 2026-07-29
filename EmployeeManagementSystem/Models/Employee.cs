d6b348e0bc317b0ba36f82d808afcba2e836fc6e
Ahmed Adel Hassan <aa3452000@gmail.com>
2026-07-29 05:29:36 +0300

    Add project files.

---
+++ EmployeeManagementSystem/Models/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime HiringDate { get; }
+        public int DepartmentId { get; set; }
+        public decimal Salary { get; set; }
+
+        public HashSet<string> Skills { get; } = new(StringComparer.OrdinalIgnoreCase);
+
+        public Employee(int id, string name, int departmentId, decimal salary) 
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException("Employee Name is required. ");
+
+            if (salary < 0)
+                throw new ArgumentException("Salary cannot be negative. ");
+
+            Id = id;
+            Name = name;
+            DepartmentId = departmentId;
+            Salary = salary;
+            HiringDate = DateTime.Now;
+            
+        }   
+
+        public virtual string GetInfo()
+        {
+            return $"ID: {Id}, Name: {Name} -- Department ID: {DepartmentId}, Salary: {Salary}";
+        }
+    }
+}
---
+++ EmployeeManagementSystem/Models/Manager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class Manager : Employee
+    {
+        public List<Employee> TeamMembers { get; } = new();
+
+        public Manager(
+            int id,
+            string name,
+            int departmentId,
+            decimal salary) : base(id, name, departmentId, salary) { }
+
+        public override string GetInfo()
+        {
+            return $"Manager - {base.GetInfo()},Team Members: {TeamMembers.Count}";
+        }
+
+        public void AddTeamMember(Employee employee)
+        {
+            if (employee is null)
+                throw new ArgumentNullException(nameof(employee));
+
+            foreach (Employee teamMember in TeamMembers)
+            {
+                if (teamMember.Id == employee.Id)
+                {
+                    throw new ArgumentException("Employee is already in this manager's team");
+                }
+            }
+            TeamMembers.Add(employee);
+        }
+        
+        
+    }
+}
